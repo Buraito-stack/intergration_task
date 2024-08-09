f92eb72e5a6e9eb49299511118c60f23fd550e71
Buraito-stack <bryantedyluke@gmail.com>
2024-08-09 16:21:38 +0700

    "Formatted PHP code in Category.php and Database.php files, adding whitespace and fixing indentation."

--- Category.php
+++ Category.php
@@ -2,35 +2,41 @@
 
 require_once './Database.php';
 
-class Category {
+class Category 
+{
     private $db;
 
-    public function __construct() {
+    public function __construct() 
+    {
         $this->db = (new Database())->getConnection();
     }
 
-    public function insert($name) {
+    public function insert($name) 
+    {
         $stmt = $this->db->prepare("INSERT INTO categories (name) VALUES (?)");
         $stmt->bind_param("s", $name);
         $stmt->execute();
         $stmt->close();
     }
 
-    public function update($id, $name) {
+    public function update($id, $name) 
+    {
         $stmt = $this->db->prepare("UPDATE categories SET name = ? WHERE id = ?");
         $stmt->bind_param("si", $name, $id);
         $stmt->execute();
         $stmt->close();
     }
 
-    public function delete($id) {
+    public function delete($id) 
+    {
         $stmt = $this->db->prepare("DELETE FROM categories WHERE id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $stmt->close();
     }
 
-    public function selectAll() {
+    public function selectAll() 
+    {
         $result = $this->db->query("SELECT * FROM categories");
         return $result->fetch_all(MYSQLI_ASSOC);
     }
--- Database.php
+++ Database.php
@@ -1,31 +1,38 @@
 <?php
 
-class Database {
-    private $host = 'localhost';
+class Database 
+{
+    private $host     = 'localhost';
     private $username = 'root';
     private $password = ''; 
     private $database = 'demo_intern';
     private $connection;
 
-    public function __construct() {
+    public function __construct() 
+    {
         $this->connect();
     }
 
-    private function connect() {
+    private function connect() 
+    {
         $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 
         if ($this->connection->connect_error) {
             $GLOBALS['db_error'] = "Connection failed: " . $this->connection->connect_error;
-        } else {
+        }
+
+        {
             $GLOBALS['db_success'] = "Connected successfully";
         }
     }
 
-    public function getConnection() {
+    public function getConnection() 
+    {
         return $this->connection;
     }
 
-    public function getConnectionStatus() {
+    public function getConnectionStatus() 
+    {
         return [
             'error' => $GLOBALS['db_error'] ?? '',
             'success' => $GLOBALS['db_success'] ?? ''
